ada924916b7b9a0a32381b9eef52b13d24e5b8b5
imransagor <imransagor22@yahoo.com>
2018-10-17 09:27:40 +0600

    add_category

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,14 @@ Route::get('/', 'HomeController@index');
 
 
 //backendsite.....
+Route::get('/logout','SuperAdminController@logout');
 Route::get('/admin', 'AdminController@index');
 Route::get('/dashboard', 'AdminController@show_dashboard');
 Route::post('/admin-dashboard','AdminController@dashboard');
+
+
+//Catagory related route
+Route::get('/add-category','CatagoryController@index');
+Route::get('/all-category','CatagoryController@all_category');
+Route::post('/save-category','CatagoryController@save_category');
+
